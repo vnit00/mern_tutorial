2078dce9287dff8b6d2a656aceec9013b3d74253
ajinkya29git <jadhavajinkya29git@gmail.com>
2022-02-13 22:00:46 +0530

    All 4 routes set up just printing a message

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,20 @@ router.get('/',(req,res)=> {
     res.status(200).json({message:"get goals"})
 })
 
+router.post('/',(req,res)=> {
+    res.status(200).json({message:"set goal"})
+})
+
+router.put('/:id',(req,res)=> {
+    res.status(200).json({message: `update goal ${req.params.id}`})
+})
+
+router.delete('/:id',(req,res)=> {
+    res.status(200).json({message: `delete goal ${req.params.id}`})
+})
+
+router.get('/',(req,res)=> {
+    res.status(200).json({message:"get goals"})
+})
+
 module.exports = router
