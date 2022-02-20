809236f9df6dc94b32b0a0477b4519ddb77e36e4
ajinkya29git <jadhavajinkya29git@gmail.com>
2022-02-20 15:17:48 +0530

    Accepting Body Data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,6 +9,7 @@ const getGoals = (req,res) => {
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req,res) => {
+    console.log(req.body)
     res.status(200).json({message:"set goal"})
 }
 
