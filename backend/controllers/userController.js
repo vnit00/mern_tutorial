220d79afc722074b8bb487876b49770993abdfe9
ajinkya29git <jadhavajinkya29git@gmail.com>
2022-03-02 23:08:04 +0530

    Hash password implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,7 +22,27 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists')
   }
 
-  res.json({message:'Register User'})
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc    Authenticate a user
